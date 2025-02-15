e4ba31508e17f8c28681232be30af49a9b92c2f0
talana tefo <talanatefo@gmail.com>
2025-02-15 11:16:46 +0200

    added quiz

--- quiz.js
+++ quiz.js
@@ -1,22 +1,18 @@
-let correctAnswer = "4";
-let radioButton1 = document.getElementById("choice1");
-
-let radioButton2 = document.getElementById("choice2");
-
-let radioButton3 = document.getElementById("choice3");
-
-let userAnswer = [radioButton1, radioButton2, radioButton3];
 function checkAnswer() {
-  const feedback = document.getElementById("feedback");
-  if (userAnswer[0].value === correctAnswer && userAnswer[0].checked == true) {
-    feedback.textContent = "Correct! Well done.";
-  } else if (
-    userAnswer[1].value !== correctAnswer &&
-    userAnswer[1].checked == true
-  ) {
-    feedback.textContent = "That's incorrect. Try again!";
-  } else if (userAnswer[2].checked == true && userAnswer[2].checked == true) {
-    feedback.textContent = "That's incorrect. Try again!";
+  let radioButtons = document.getElementsByName("quiz");
+  let correctAnswer = "4";
+  let userAnswer = null;
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+      userAnswer = radioButtons[i].value;
+    }
+
+    let feedback = document.getElementById("feedback");
+    if (userAnswer == correctAnswer) {
+      feedback.textContent = "Correct! Well done.";
+    } else if (userAnswer !== correctAnswer) {
+      feedback.textContent = "That's incorrect. Try again!";
+    }
   }
 }
 
